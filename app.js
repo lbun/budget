0d2072a3bf942ace733c5c9c6c4914ccc0c0d4d8
luigibungaro <luigi.bungaro@gmail.com>
2020-04-30 10:42:06 +0200

    implemented month

--- app.js
+++ app.js
@@ -150,7 +150,8 @@ var UIController = (function () {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
 
     }
     // PRIVATE METHOD
@@ -267,6 +268,21 @@ var UIController = (function () {
 
         },
 
+        displayMonth: function () {
+            
+            var now, months,  month, year;
+
+            var now = new Date();
+            // var christmas = new Date(2016, 11, 25):
+
+            year = now.getFullYear();
+
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+
+        },
+
  
         getDOMstrings: function() {
             return DOMstrings;
@@ -369,6 +385,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             setupEventListeners();
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
